87ce90dd0baffc4c9d0fba16a21b2b4b9a1a6616
David Cole <davidtcole@yahoo.com>
2020-08-10 16:40:28 -0700

    add some last second changes to remove console.logs

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,27 @@ let cipherSentence = "";
 
 function getUserInput() {
   userSentence = prompt("Please enter a sentence to encode:");
-  console.log(userSentence);
   capitalizeFirstAndLast(userSentence);
 }
 
 function capitalizeFirstAndLast(userSentence) {
   const firstAndLastLetter = userSentence.charAt(0) + userSentence.charAt(userSentence.length - 1);
-  console.log(firstAndLastLetter);
   reverseLetters(firstAndLastLetter.toUpperCase(), userSentence);
 }
 
 function reverseLetters(firstAndLastLetter, userSentence) {
   const combinedSentence = userSentence + (firstAndLastLetter.split("").reverse().join(""));
-  console.log(combinedSentence);
   insertLetter(userSentence, combinedSentence);
 }
 
 function insertLetter(userSentence, combinedSentence) {
   const letterToRemove = Math.floor((userSentence.length / 2));
   const result = userSentence.charAt(letterToRemove) + combinedSentence;
-  console.log(result);
   reverseSentence(result);
 }
 
 function reverseSentence(result) {
   cipherSentence = (result.split("").reverse().join(""));
-  console.log(cipherSentence);
 }
 
 getUserInput();
